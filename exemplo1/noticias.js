d9b1e60458c6c959141b57f0114c9ffc2452b5c6
brunosetra <bruno.setra@gmail.com>
2018-06-06 15:11:01 -0300

    Aula 9 - Respondendo requisições com base na url requisitada

--- exemplo1/noticias.js
+++ exemplo1/noticias.js
@@ -1,7 +1,16 @@
 var http = require('http');
 
 http.createServer( (req,res)=> {
-    res.end("<html><body>Portal de Noticias</body></html>");
+    
+    if (req.url === '/tecnologia') {
+        res.end("<html><body>Portal de Noticias de Tecnologia</body></html>");
+    } else if (req.url === '/moda') {
+        res.end("<html><body>Portal de Noticias de Moda</body></html>");
+    } else if (req.url === '/games') {
+        res.end("<html><body>Portal de Noticias de Games</body></html>");
+    } else {
+        res.end("<html><body>Portal de Noticias</body></html>");
+    }
     
 }).listen(3000);
 
