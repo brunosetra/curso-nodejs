39b5a9c9135e051340587e72a0076ef8f46bb5c5
brunosetra <bruno.setra@gmail.com>
2018-06-13 18:56:15 -0300

    Seção: 7 Models e ganhando produtividade com Consign (sucessor do Express-Load)

--- app/routes/noticia.js
+++ app/routes/noticia.js
@@ -1,14 +1,16 @@
 module.exports = (app) => {
 
-    app.get('/noticias', (req, res) => {
+    
+    app.get('/noticia', (req, res) => {
         
         var conn = app.config.dbConnection();
 
         var noticiasModel = app.app.models.noticiasModel
         
-        noticiasModel.getNoticias(conn, (erro, result) => {
-            res.render('noticias/noticias', { noticias: result })
+        noticiasModel.getNoticia(conn, (erro, result) => {
+            res.render('noticias/noticia', { noticias: result })
         })
+        
         
     })
     
--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,10 +1,18 @@
 var mysql = require('mysql')
 
-module.exports = ()=> {
+var connMySQL = ()=>{
+    console.log('conexao db estabelecida')
+
     return mysql.createConnection({
         host : '172.31.160.36',
         user : 'root',
         password: '1q2w3e4r',
         database : 'portal_noticias'
     })
+
+}
+
+module.exports = ()=> {
+    console.log('dbConn init')
+    return connMySQL;
 }
