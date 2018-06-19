2bf7234873397b5de87ebaf82e51fa5f917fd669
brunosetra <bruno.setra@gmail.com>
2018-06-19 16:12:19 -0300

    Seção: 10 Ganhando produtividade com o Express Validator

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,15 +1,34 @@
 module.exports = (app) => {
     app.get('/formulario_inclusao_noticia', (req,res)=> {
-        res.render('admin/form_add_noticia')
+        res.render('admin/form_add_noticia', {validacao : {}, noticia : {}})
     })
 
     app.post('/noticias/salvar', (req,res)=>{
         var noticia = req.body
 
-        var conn = app.config.dbConnection();
-        var noticiasModel = app.app.models.noticiasModel
+        req.assert('titulo','Titulo Obrigatorio').notEmpty();
+        req.assert('resumo','Resumo Obrigatorio').notEmpty();
+        req.assert('resumo','Resumo deve conter entre 10 e 100 caracteres').len(10,100);
+        req.assert('autor','Autor Obrigatorio').notEmpty();
+        req.assert('data_noticia', 'Data é obrigatório').notEmpty().isISO8601();
+        req.assert('noticia','Noticia Obrigatorio').notEmpty();
 
-        noticiasModel.salvarNoticia(noticia, conn, (error,result)=>{
+        var erros = req.validationErrors()
+
+        console.log('Log de erro ao inserir noticia. ', erros)
+
+        if (erros){
+            res.render('admin/form_add_noticia', {validacao : erros, noticia : noticia})
+            return
+        }
+
+        var conn = app.config.dbConnection();
+        var noticiasModel = new app.app.models.NoticiasDAO(conn)
+
+        noticiasModel.salvarNoticia(noticia, (error,result)=>{
+
+
+
             res.redirect('/noticias')
         })
 
--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -4,12 +4,26 @@ module.exports = (app) => {
         
         var conn = app.config.dbConnection();
 
-        var noticiasModel = app.app.models.noticiasModel
+        var noticiasModel = new app.app.models.NoticiasDAO(conn)
         
-        noticiasModel.getNoticias(conn, (erro, result) => {
+        noticiasModel.getNoticias((erro, result) => {
             res.render('noticias/noticias', { noticias: result })
         })
         
     })
+
+    app.get('/noticia', (req, res) => {
+        
+        var conn = app.config.dbConnection();
+
+        var noticiasModel = new app.app.models.NoticiasDAO(conn)
+        
+        noticiasModel.getNoticia((erro, result) => {
+            res.render('noticias/noticia', { noticias: result })
+        })
+        
+        
+    })
+    
     
 }
