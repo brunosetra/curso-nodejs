58baddf9c00495a35f57e38cd40e2b775ad26dbf
brunosetra <bruno.setra@gmail.com>
2018-06-20 10:54:59 -0300

    Seção: 12 Utilizando recursos estáticos como imagens, arquivos de estilo e scripts

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,3 +1,11 @@
-module.exports.index = function(appliction, req, res){
-    res.render('home/index');
+module.exports.index = function(application, req, res){
+
+    var conn = application.config.dbConnection()
+    var noticiasModel = new application.app.models.NoticiasDAO(conn)
+
+    noticiasModel.get5UltimasNoticias(function(error, result){
+
+        res.render('home/index', {noticias: result});
+    })
+
 }
